01c11f8e124fe03620da627acb5a0162f4526d26
Conor <cdu58@uclive.ac.nz>
2024-09-30 12:18:19 +1300

    Revise code and make tests

--- main.cpp
+++ main.cpp
@@ -10,165 +10,131 @@
     #include <unistd.h>
 #endif
 
-
-//#include <chrono>
-#include <iostream>
-
-// #include "implementations/poisson_multithread.h"
-#include "implementations/poisson_r2.h"
-// #include "implementations/poisson.h"
-
 #define PRINT_THRESHOLD 15
 
-/**
- * poisson.c
- * Implementation of a Poisson solver with Dirichlet boundary conditions.
- *
- * This template handles the basic program launch, argument parsing, and memory
- * allocation required to implement the solver *at its most basic level*. You
- * will likely need to allocate more memory, add threading support, account for
- * cache locality, etc...
- *
- * BUILDING:
- * gcc -o poisson poisson.c -lpthread
- *
- * [note: linking pthread isn't strictly needed until you add your
- *        multithreading code]
- *
- * TODO:
- * 1 - Read through this example, understand what it does and what it gives you
- *     to work with.
- * 2 - Implement the basic algorithm and get a correct output.
- * 3 - Add a timer to track how long your execution takes.
- * 4 - Profile your solution and identify weaknesses.
- * 5 - Improve it!
- * 6 - Remember that this is now *your* code and *you* should modify it however
- *     needed to solve the assignment.
- *
- * See the lab notes for a guide on profiling and an introduction to
- * multithreading (see also threads.c which is reference by the lab notes).
- */
-
-
- // Global flag
- // Set to true when operating in debug mode to enable verbose logging
 bool debug = true;
 
-void print_slice(int n, double* data, int ex_size = 0) {
+static void build_boundary(double* buf, int32_t n) {
+    const int32_t ln_s = n + 2;
+    const int32_t pl_s = (n + 2) * (n + 2);
+    
+    for (int32_t i = 1; i <= n; i++) {
+        for (int32_t j = 1; j <= n; j++) {
+            buf[ln_s * (i + 1) + (j + 1) + pl_s] = -1.0; // Bottom
+            buf[ln_s * (i + 1) + (j + 1) + pl_s * (n + 1)] = 1.0; // Top
+        }
+    }
+}
 
-    uint32_t val_add = ex_size / 2;
+double* poisson_mixed_r2(const int n, double* const source, const int iterations, const double delta) {
+    if (debug) {
+        printf("Starting solver with:\n"
+               "n = %i\n"
+               "iterations = %i\n"
+               "delta = %f\n", n, iterations, delta);
+    }
 
-    for (int x = 0; x < n; ++x)
-    {
-        for (int y = 0; y < n; ++y)
-        {
-            // printf("%0.5f ", result[((n / 2) * n + y) * n + x]);
-            printf("%0.5f ", data[(((n + ex_size) / 2) * (n + ex_size) + (y + val_add)) * (n + ex_size) + (x+val_add)]);
+    size_t buf_size = (n + 2) * (n + 2) * (n + 2);
+    double* curr = (double*)calloc(buf_size, sizeof(double));
+    double* next = (double*)calloc(buf_size, sizeof(double));
+
+    if (!curr || !next) {
+        fprintf(stderr, "Error: ran out of memory when trying to allocate %i sized cube\n", n);
+        exit(EXIT_FAILURE);
+    }
+
+    const double delta_sq = delta * delta;
+    const double one_sixth = 1.0 / 6.0;
+
+    for (int iter = 0; iter < iterations; iter++) {
+        build_boundary(curr, n);
+        for (int k = 1; k <= n; k++) {
+            for (int j = 1; j <= n; j++) {
+                double* row_b = curr + ((k * (n + 2) + j) * (n + 2));
+                double* row_f = curr + ((k * (n + 2) + j) * (n + 2) + 2);
+                double* src_row = source + ((k - 1) * n + (j - 1) * n);
+
+                for (int i = 0; i < n; i++) {
+                    double res = row_b[i] + row_f[i] + 
+                                 curr[((k + 1) * (n + 2) + j) * (n + 2) + i + 1] +
+                                 curr[((k - 1) * (n + 2) + j) * (n + 2) + i + 1] +
+                                 curr[(k * (n + 2) + j + 1) * (n + 2) + i + 1] +
+                                 curr[(k * (n + 2) + j - 1) * (n + 2) + i + 1];
+
+                    next[(k * (n + 2) + j) * (n + 2) + i + 1] = one_sixth * (res - (delta_sq * src_row[i]));
+                }
+            }
+        }
+
+        double* temp = curr;
+        curr = next;
+        next = temp;
+    }
+
+    free(next);
+    return curr;
+}
+
+void print_slice(int n, double* data) {
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < n; ++y) {
+            printf("%0.5f ", data[((n / 2) * (n + 2) + y + 1) * (n + 2) + (x + 1)]);
         }
         printf("\n");
     }
 }
-#ifdef _DEBUG
-    debug = true;
-#endif // DEBUG
 
-int main(int argc, char** argv)
-{
-    // Default settings for solver
-    int iterations = 10;
-    int n = 5;
-    int threads = 1;
-    float delta = 1;
-    int x = -1;
-    int y = -1;
-    int z = -1;
+int main(int argc, char** argv) {
+    int iterations = 10, n = 5;
+    double delta = 1;
     double amplitude = 1.0;
+    int x = -1, y = -1, z = -1;
 
     int opt;
-
-    // parse the command line arguments
-    while ((opt = getopt(argc, argv, "h:n:i:x:y:z:a:t:d:")) != -1)
-    {
-        switch (opt)
-        {
-        case 'h':
-            printf("Usage: poisson [-n size] [-x source x-poisition] [-y source y-position] [-z source z-position] [-a source amplitude] [-i iterations] [-t threads] [-d] (for debug mode)\n");
-            return EXIT_SUCCESS;
-        case 'n':
-            n = atoi(optarg);
-            break;
-        case 'i':
-            iterations = atoi(optarg);
-            break;
-        case 'x':
-            x = atoi(optarg);
-            break;
-        case 'y':
-            y = atoi(optarg);
-            break;
-        case 'z':
-            z = atoi(optarg);
-            break;
-        case 'a':
-            amplitude = atof(optarg);
-            break;
-        case 't':
-            threads = atoi(optarg);
-            break;
-        case 'd':
-            debug = true;
-            break;
-        default:
-            fprintf(stderr, "Usage: poisson [-n size] [-x source x-poisition] [-y source y-position] [-z source z-position] [-a source amplitude]  [-i iterations] [-t threads] [-d] (for debug mode)\n");
-            exit(EXIT_FAILURE);
+    while ((opt = getopt(argc, argv, "h:n:i:x:y:z:a:d:")) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("Usage: poisson [-n size] [-x source x-poisition] [-y source y-position] [-z source z-position] [-a source amplitude] [-i iterations] [-d] (for debug mode)\n");
+                return EXIT_SUCCESS;
+            case 'n': n = atoi(optarg); break;
+            case 'i': iterations = atoi(optarg); break;
+            case 'x': x = atoi(optarg); break;
+            case 'y': y = atoi(optarg); break;
+            case 'z': z = atoi(optarg); break;
+            case 'a': amplitude = atof(optarg); break;
+            case 'd': debug = true; break;
+            default: fprintf(stderr, "Usage: poisson [-n size] [-x source x-poisition] [-y source y-position] [-z source z-position] [-a source amplitude] [-i iterations] [-d] (for debug mode)\n"); exit(EXIT_FAILURE);
         }
     }
 
-    // Ensure we have an odd sized cube
-    if (n % 2 == 0)
-    {
+    if (n % 2 == 0) {
         fprintf(stderr, "Error: n should be an odd number!\n");
         return EXIT_FAILURE;
     }
 
-    // Create a source term with a single point in the centre
     double* source = (double*)calloc(n * n * n, sizeof(double));
-    if (source == NULL)
-    {
-        fprintf(stderr, "Error: failed to allocated source term (n=%i)\n", n);
+    if (!source) {
+        fprintf(stderr, "Error: failed to allocate source term (n=%i)\n", n);
         return EXIT_FAILURE;
     }
 
-    // Default x,y, z
-    if (x < 0 || x > n - 1)
-        x = n / 2;
-    if (y < 0 || y > n - 1)
-        y = n / 2;
-    if (z < 0 || z > n - 1)
-        z = n / 2;
+    if (x < 0) x = n / 2;
+    if (y < 0) y = n / 2;
+    if (z < 0) z = n / 2;
 
     source[(z * n + y) * n + x] = amplitude;
 
-
-    //std::chrono::time_point time_start_r2 = std::chrono::high_resolution_clock::now();
-
-    // Calculate the resulting field with Dirichlet conditions
     double* result_r2 = poisson_mixed_r2(n, source, iterations, delta);
 
-    //td::chrono::time_point time_end_r2 = std::chrono::high_resolution_clock::now();
-
     if (debug) {
-        //std::chrono::duration<double> time_diff_r2 = time_end_r2 - time_start_r2;
-        //std::cout << "Time taken: " << time_diff_r2.count() << "\n";
+        printf("Finished solving.\n");
     }
 
     if (n <= PRINT_THRESHOLD) {
-        print_slice(n, result_r2, 2);
+        print_slice(n, result_r2);
     }
 
     free(source);
     free(result_r2);
-
     return EXIT_SUCCESS;
 }
-
